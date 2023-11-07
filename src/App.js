b3d0034fc4696cc2efb452c3f1f0f21d1f89c178
midcondria <hyukkind@naver.com>
2023-11-07 23:48:47 +0900

    feature : 가위바위보 미니 게임
    - 리액트 공부용 가벼운 예제

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Button from './Button.js';
+import Board from "./Board";
+
+function random(n) {
+    return Math.ceil(Math.random() * n);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>안녕 리액트!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [myHistory, setMyHistory] = useState([]);
+    const [otherHistory, setOtherHistory] = useState([]);
+
+    const handleRollClick = () => {
+        const nextMyNum = random(3);
+        const nextOtherNum = random(3);
+        setMyHistory([...myHistory, nextMyNum]);
+        setOtherHistory([...otherHistory, nextOtherNum]);
+    };
+
+    const handleClearClick = () => {
+        setMyHistory([]);
+        setOtherHistory([]);
+    };
+
+    return(
+        <div>
+            <div>
+                <Button onClick={handleRollClick}>던지기</Button>
+                <Button onClick={handleClearClick}>처음부터</Button>
+            </div>
+            <div>
+                <Board name="나" color="blue" gameHistory={myHistory} />
+                <Board name="상대" color="red" gameHistory={otherHistory} />
+            </div>
+        </div>
+    )
 }
 
 export default App;
