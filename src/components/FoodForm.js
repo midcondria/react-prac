13c686b95520485e5ef7111ff8d9ea4b9772e0cc
midcondria <hyukkind@naver.com>
2023-12-20 18:45:35 +0900

    feature : 파일 인풋 추가
    비제어 컴포넌트 연습용

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import FileInput from "./FileInput";
 
-function validate(type, value) {
+function sanitize(type, value) {
   switch (type) {
     case "number":
       return Number(value) || 0;
@@ -12,38 +13,48 @@ function validate(type, value) {
 
 function FoodForm() {
   const [values, setValues] = useState({
+    imgFile: null,
     title: "",
     calorie: 0,
     content: "",
   });
-
-  const handleChange = (e) => {
-    const { name, value, type } = e.target;
-    setValues((prevValues) => ({
-      ...prevValues,
-      [name]: validate(type, value),
-    }));
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values);
   };
 
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+    handleChange(name, sanitize(type, value));
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-      <input name="title" value={values.title} onChange={handleChange}></input>
+      <FileInput
+        name="imgFile"
+        value={values.imgFile}
+        onChange={handleChange}
+      />
+      <input name="title" value={values.title} onChange={handleInputChange} />
       <input
         type="number"
         name="calorie"
         value={values.calorie}
-        onChange={handleChange}
-      ></input>
+        onChange={handleInputChange}
+      />
       <input
         name="content"
         value={values.content}
-        onChange={handleChange}
-      ></input>
+        onChange={handleInputChange}
+      />
       <button type="submit">확인</button>
     </form>
   );
