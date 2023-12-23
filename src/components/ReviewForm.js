dc4a6f224a42e548a72ca3d163ea8db64553755e
midcondria <hyukkind@naver.com>
2023-12-23 21:58:43 +0900

    feature : 점수 매기기 기능 추가 및 CSS 적용

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./ReviewForm.css";
 import FileInput from "./FileInput";
+import RatingInput from "./RatingInput";
 
 function ReviewForm() {
   const [values, setValues] = useState({
@@ -35,7 +36,7 @@ function ReviewForm() {
         onChange={handleChange}
       />
       <input name="title" value={values.title} onChange={handleInputChange} />
-      <input
+      <RatingInput
         name="rating"
         type="number"
         value={values.rating}
