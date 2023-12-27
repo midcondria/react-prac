e13c4ae2b101cb352f6bab21444bb122f2f0a85f
midcondria <hyukkind@naver.com>
2023-12-27 15:55:30 +0900

    feature : 다국어 기능 v2
    - 버튼에 다국어 적용

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -3,6 +3,7 @@ import "./ReviewForm.css";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
 import useAsync from "./hooks/useAsync";
+import useTranslate from "./hooks/useTranslate";
 
 const INITIAL_VALUES = {
   title: "",
@@ -18,6 +19,7 @@ function ReviewForm({
   onSubmit,
   onCancel,
 }) {
+  const t = useTranslate();
   const [values, setValues] = useState(initialValues);
   const [isSubmitting, submittingError, onSubmitAsync] = useAsync(onSubmit);
 
@@ -70,9 +72,9 @@ function ReviewForm({
         onChange={handleInputChange}
       />
       <button type="submit" disabled={isSubmitting}>
-        확인
+        {t("confirm button")}
       </button>
-      {onCancel && <button onClick={onCancel}>취소</button>}
+      {onCancel && <button onClick={onCancel}>{t("cancel button")}</button>}
       {submittingError?.message && <div>{submittingError.message}</div>}
     </form>
   );
